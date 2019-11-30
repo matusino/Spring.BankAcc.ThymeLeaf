1d174b0cb26866ed011ada956ba59e9974c79f45
matus <matusino.sykora@gmail.com>
2019-11-30 16:48:02 +0100

    Initial commit

---
+++ src/test/java/com/sykora/Spring/BankAcc/ThymeLeaf/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sykora.Spring.BankAcc.ThymeLeaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
